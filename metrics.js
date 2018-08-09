f968148ed449ecf1dec5778c80e4baf5cee3ad0c
convoitise EX <potetsia@enst.fr>
2018-08-09 20:02:34 +0200

    initial commit of proposed metrics file

---
+++ metrics.js
@@ -0,0 +1,20 @@
+'use strict';
+/**
+ * This file contains the definition of the Metrics object, that is present in 
+ *  all recording sets and updates during playback
+ * 
+ */
+function Metrics() {
+    //Image-based objective
+    this.bluriness = 0;
+    this.brightness = 0;
+    //Sensor-based
+    this.shakiness = 0;
+    this.FOV.orientation =0;
+    this.FOV.tilt =0;
+    //proposed
+    this.facing = 0;
+    //Characteristics
+    this.bitrate = 0;
+    this.resolution =0;
+}
