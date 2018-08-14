60ad355e94c1c3b3f024563035ea0ec87068de30
convoitise EX <potetsia@enst.fr>
2018-08-14 20:14:08 +0200

    added script to generate blur estimation from frames

---
+++ tools/generate_blur_descriptors.py
@@ -0,0 +1,46 @@
+from measure_blur import get_blur_metric
+import os
+import json
+
+filenames = [
+    '20140325_121238_720p_1800k', '20140325_121245_720p_1800k', '20140325_131253_720p_1800k', 'A002C001_140325E3_720p_1800k',
+    'IMG_0367_720p_1800k', 'Take5_Nexus5_720p_1800k', 'VID_20140325_131247_720p_1800k', 'VID_20140325_131249_720p_1800k'
+]
+separator = '_'
+start_n = 0
+end_n = 999
+formater = '03d'
+extension = '.bmp'
+OUTPUTDIR = 'test'
+file_out_suffix = '_BLUR'
+
+def get_em(filename):
+    blurs = []
+    for i in range(start_n, end_n):
+        blur = get_blur_metric(filename + separator + format(i, formater) + extension)
+        if blur is None:
+            print('Done at ' + (filename + separator + format(i, formater) + extension))
+            return blurs
+        else:
+            blur['PresentationTime'] = i * 1000    #in ms
+            blur['Blur'] = blur
+            blurs.append(copy.copy(blur))
+
+
+def flush_json_to_file_out(filename, data):
+    if not os.path.exists(OUTPUTDIR):
+        print(os.mkdir(OUTPUTDIR))
+    with open(os.getcwd() + '/' + OUTPUTDIR + '/' + filename, 'w+') as f:
+        json.dump(data, f)
+        print('written at ' + os.getcwd() + '/' + OUTPUTDIR + '/' + filename)
+
+def main():
+    for filename in filenames:
+        blur = get_em(filename)
+        flush_json_to_file_out(filename + file_out_suffix + '.json', blur)
+    input('continue?')
+	#That's All Folks!
+    exit(0)
+
+if __name__ == '__main__':
+    main()
