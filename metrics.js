faa12345c215ada6613bb2c1424a9021d9742afa
convoitise EX <potetsia@enst.fr>
2018-10-04 10:47:57 +0200

    implemented getScore, with currently available metrics

--- metrics.js
+++ metrics.js
@@ -35,6 +35,60 @@ function printMetrics(index_in) {
     logUI('Showing metrics for set at index: ' + index_in + ' at video time' + p.v.currentTime +
         ' Blur Metric (for frame: ' + temp_frame + '): ' + temp_set.imageQSet[temp_frame].Blur +
         'Tilt: ' + temp_set.lastOrientation['Y'] + '  Skew: ' + temp_set.lastOrientation['Z'] + //tilt should be around 0deg
-        'Orientation: ' + temp_set.lastOrientation['X'] + '  Bearing: ' + getBearing(temp_set) + '   Distance: ' + getDistance(temp_set)
+        'Orientation: ' + temp_set.lastOrientation['X'] + '  Bearing: ' + getBearing(temp_set) +
+        '   Distance: ' + getDistance(temp_set) + '     __Score:_' + getScore(index_in)
     );
+}
+
+function getScore(index_in) {
+    let temp_frame = parseInt(p.v.currentTime - globalSetIndex[index_in].descriptor.tDiffwReferenceMs / 1000);
+    let temp_set = globalSetIndex[index_in];
+
+    //Shakiness
+    let Ss = 1; //TODO shakiness not implemented yet
+    let SSs = 1 - Ss;
+
+    //Tilt
+    let St;
+    if (Math.abs(temp_set.lastOrientation['Y']) > maxTilt) {
+        St = 1;
+    } else {
+        St = mapToRange(Math.abs(temp_set.lastOrientation['Y']), 0, maxTilt, 0, 1);
+    }
+    let SSt = 1 - St;
+
+    //Bitrate
+    let Vb = 0;
+    let Bmin = 999999;
+    let Bmax = 0;
+    temp_set.mpd.representations.forEach((rep) => {
+        let b = Number(rep.bandwidth);
+        if (b < Bmin) {
+            Bmin = b;
+        } else if (b > Bmax) {
+            Bmax = b;
+        }
+    });
+    Vb = mapToRange(
+        Number(
+            temp_set.mpd.representations[temp_set.active_representation].bandwidth
+        ), Bmin, Bmax, 0, 1);
+
+    //Image Q
+    let ImQmin = 0;
+    let ImQmax = 0;
+    //TODO calculate this once per event update, not per request / per stream
+    for (let i = 0; i < globalSetIndex.length; i++) {
+        let t_f = parseInt(p.v.currentTime - globalSetIndex[i].descriptor.tDiffwReferenceMs / 1000);
+        let b = globalSetIndex[i].imageQSet[t_f].Blur;
+        //We keep ImQmin to 0, because we can have a frame with no edges at some point (but not now)
+        //ImQmin = (ImQmin > b) ? b : ImQmin;
+        ImQmax = (ImQmax < b) ? b : ImQmax;
+    }
+    let Iq = mapToRange(temp_set.imageQSet[temp_frame].Blur, ImQmin, ImQmax, 0, 1);
+
+
+    //    let SSs = a1 * (1 - Ss);
+    //    let SSt = a1 * (1 - St);
+
 }
