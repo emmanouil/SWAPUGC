6d9df535acab569ea72b4252eef64e777d19303f
emmanouil <potetsia@enst.fr>
2018-10-25 19:30:16 +0200

    added the Scene object

--- policies.js
+++ policies.js
@@ -1,7 +1,38 @@
 "use strict";
 /* global globalSetIndex */ //standard supported by Firefox and Chrome (not IE)
 /* global sourceBuffer */ //in mse.js
-var rankedStreams = [];
+var rankedStreams = []; //it is updated with the streams each time a new score is calculated
+var scenes = []; //holds the scenes
+
+
+/**
+ * Scene object, used for cinematic criteria
+ */
+function Scene(cur_t, new_id, old_t, old_id) {
+    this.startTime = cur_t;
+    this.currentStreamId = new_id;
+    this.previousStreamId = old_id;
+    this.overridden = false;    //indicates that a cinematic criterion was overridden
+    this.duration = 0;
+    this.endTime = 0;
+}
+
+Scene.prototype = {
+    set endScene(endtime) {
+        this.duration = endtime - this.startTime;
+        this.endTime = endtime;
+    }
+};
+
+function newScene(cur_t, new_id, old_t, old_id) {
+    let tmp = scenes.push(new Scene(cur_t, new_id, old_t, old_id));
+    let i = tmp - 1;
+    if (i) {
+        scenes[i].endScene(cur_t);
+    }
+
+}
+
 
 function filterStreams() {
     let filteredStreams = [];
